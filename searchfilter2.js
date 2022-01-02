eafa19d53dfbf5b429ce536d3abc24bdf4a81372
Slamet-Kevin <95071615+Slamet-Kevin@users.noreply.github.com>
2022-01-02 10:51:29 +0700

    Add files via upload

---
+++ searchfilter2.js
@@ -0,0 +1,33 @@
+/* SEARCHFILTER 2 dari http://w3schools-fa.ir/w3js/tryitbb78.html?filename=tryw3js_filters_table dan www.w3schools.com/w3js/tryit.asp?filename=tryw3js_filters_table*/
+var w3 = {};
+w3.getElements = function (id) {
+  if (typeof id == "object") {
+    return [id];
+  } else {
+    return document.querySelectorAll(id);
+  }
+};
+w3.filterHTML = function(id, sel, filter) {
+  var a, b, c, i, ii, iii, hit;
+  a = w3.getElements(id);
+  for (i = 0; i < a.length; i++) {
+    b = a[i].querySelectorAll(sel);
+    for (ii = 0; ii < b.length; ii++) {
+      hit = 0;
+      if (b[ii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+        hit = 1;
+      }
+      c = b[ii].getElementsByTagName("*");
+      for (iii = 0; iii < c.length; iii++) {
+        if (c[iii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+          hit = 1;
+        }
+      }
+      if (hit == 1) {
+        b[ii].style.display = "";
+      } else {
+        b[ii].style.display = "none";
+      }
+    }
+  }
+};
